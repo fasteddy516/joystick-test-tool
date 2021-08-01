1022ee8f2ce8e19f0236e0af111739bee7032fd0
Edward Wright <fasteddy@thewrightspace.net>
2021-08-01 13:57:30 -0400

    Initial Monogame template

---
+++ joystick-test-tool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace joystick_test_tool
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
